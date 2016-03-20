0660db017873feffaa3c6ecec24a44411d3270f4
xelda <patrik.gpost@gmail.com>
2016-03-20 05:42:04 -1100

    1

--- loginForm/newViewController.swift
+++ loginForm/newViewController.swift
@@ -9,13 +9,52 @@
 import UIKit
 
 class newViewController: UIViewController {
+    
+    @IBOutlet weak var loadNSUserDefaults: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        let sliderValue = NSUserDefaults.standardUserDefaults().floatForKey("slider_value") //LOAD NSUserDefaults
+        
+    loadNSUserDefaults.text = "\(sliderValue)"
+        
+        
+        
+    }
 
 
     @IBOutlet weak var labelSliderOne: UILabel!
     
+      
+    
+   
+    @IBAction func valueChangeEnded(sender: UISlider) {
+        print("save to user defaults")
+        NSUserDefaults.standardUserDefaults().setFloat(sender.value, forKey: "slider_value")
+            }
+    
+    
+    
+    
+   
+    
+    
+
+    
+    
+    @IBOutlet weak var loadLabel: UILabel!
+    
+  
+    
+    
     @IBAction func sliderOne(sender: UISlider) {
        
-       
+        
+       //LOAD NSUSERDEFAULTS
+        NSUserDefaults.standardUserDefaults().setFloat(sender.value, forKey: "slider_value")
+        
         let sliderInt = Int(sender.value*100)
         let sliderFloat = Float(sender.value)
         let sliderCGFloat = CGFloat(sender.value)
@@ -32,7 +71,7 @@ class newViewController: UIViewController {
         
       
         
-        labelSliderOne.text = "\(sliderInt)"
+        labelSliderOne.text = "\(sliderInt)%"
         
         
         
@@ -49,7 +88,7 @@ class newViewController: UIViewController {
     }
   
     
-   
+    
     
     
 }
